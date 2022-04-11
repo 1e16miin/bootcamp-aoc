396dec2884c76b1b10c3ffd9edc6026e1482befe
1e16miin <ddogi1513@naver.com>
2022-04-11 15:57:42 +0900

    refactor part1 and init solve part2

--- clojure/src/aoc2018_3.clj
+++ clojure/src/aoc2018_3.clj
@@ -1,9 +1,10 @@
 (ns aoc2018_3
   (:require [clojure.java.io :as io]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.set :as set]))
 
 
-(defn get-strings-from-input
+(defn puzzle-input
   [filename]
   (-> filename
       io/resource
@@ -19,8 +20,8 @@
 
 ;; # 뒤에 오는 숫자는 ID, @ 뒤에 오는 숫자 쌍 (a, b)는 시작 좌표, : 뒤에 오는 (c x d)는 격자를 나타냄.
 ;; 입력의 정보대로 격자 공간을 채우면 아래와 같이 됨.
-
 ;;      ........
+
 ;;      ...2222.
 ;;      ...2222.
 ;;      .11XX22.
@@ -31,56 +32,75 @@
 
 ;; 여기서 XX는 ID 1, 2, 3의 영역이 두번 이상 겹치는 지역.
 ;; 겹치는 지역의 갯수를 출력하시오. (위의 예시에서는 4)
-(defn get-numbers-from-string
+(defn input->fabric-info
   "string에서 숫자만 추출하는 함수
    input: #1 @ 1,3: 41x4
-   output: (1 1 3 41 4)"
+   output: {:id 1 :x 1 :y 3 :width 41 :height 4}"
   [string]
-  (let [result (re-seq (re-pattern #"\d+") string)];;re-matches
-    (mapv read-string result)))
-;; ->는 영어로 to로 읽습니다.
+  (->> string
+       (re-seq #"\d+")
+       (map read-string)
+       (zipmap [:id :x :y :width :height])))
+
+;; ->는 영어로 to로 읽습니다.`
 ;; a->b = a to b
-(defn get-point-and-range-from-numbers
-  "시작점과 범위를 구하는 함수
-   input: '(1 1 3 41 4)
-   output: [1 3 41 4]"
-  "{:x 1 :y 3 :w 41 :h 4}" ;; 맵을 리턴하는 함수로 바꿔보기 ;; rect
-  [[_id x y x-range y-range]]
-  (let [point [x y]
-        range [x-range y-range]] ;; range 말고 다른 걸루!!
-    [point range]))
 
-(defn get-coord-list ;; 맵을 받는 함수 => destructuring 이용
+(defn get-coord ;; 맵을 받는 함수 => destructuring 이용
   "시작점으로 부터 범위내의 모든 좌표를 구하는 함수
-   input: [2 2] [1 1]
-   output: ([2 2] [2 3] [3 2] [3 3])"
-  [[[x y] [width height]]]
+   input: {:id 1 :x 2 :y 2 :width 2 :height 2}
+   output: ({:id 1 :coord [2 2]} {:id 1 :coord [2 3]} {:id 1 :coord [3 2]} {:id 1 :coord [3 3]})"
+  [{:keys [id x y width height]}]
   ;; map destructing https://clojure.org/guides/destructuring#_associative_destructuring
-  (for [dx (range width) ;;(m :x)
+  (for [dx (range width)
         dy (range height)]
-    [(+ x dx) (+ y dy)]))
+    {:id id :coord [(+ x dx) (+ y dy)]}))
 
+
+(defn extract-coord
+  [lst]
+  (->> lst
+       (map #(:coord %))))
 
 (comment (->> "day3.sample.txt"
-              get-strings-from-input
-              (map get-numbers-from-string)
-              (map get-point-and-range-from-numbers)
-              (map get-coord-list) ;; hash-map
+              puzzle-input
+              (map input->fabric-info)
+              (map get-coord) ;; hash-map
+              (map extract-coord)
               (apply concat)
               frequencies
               (filter #(>= (val %) 2))
               count))
+
 ;; 파트 2
 ;; 입력대로 모든 격자를 채우고 나면, 정확히 한 ID에 해당하는 영역이 다른 어떤 영역과도 겹치지 않음
 ;; 위의 예시에서는 ID 3 이 ID 1, 2와 겹치지 않음. 3을 출력.
 ;; 겹치지 않는 영역을 가진 ID를 출력하시오. (문제에서 답이 하나만 나옴을 보장함)
 
-(comment (->> "day3.sample.txt"
-              get-strings-from-input
-              (map get-numbers-from-string)
-              (map get-point-and-range-from-numbers)
-              (map get-coord-list)
-              (apply concat)
-              frequencies
-              (filter #(>= (val %) 2))
-              count))
+
+
+(defn extract-id
+  "coordinate에 binding된 id들을 추출하는 함수
+   input: [841 660] [{:id 349, :coord [841 660]} {:id 355, :coord [841 660]}]
+   output: (349 355)
+   "
+  [[_coord coord-map-list]]
+  (->> coord-map-list
+       (map #(:id %))))
+
+(comment
+  (let [fabric-info (->> "day3.sample.txt"
+                         puzzle-input
+                         (map input->fabric-info))
+        fabric-ids (->> fabric-info
+                        (map #(:id %))
+                        set)]
+    (->> fabric-info
+         (map get-coord)
+         (apply concat)
+         (group-by :coord)
+         (filter #(>= (count (val %)) 2))
+         (map extract-id)
+         (apply concat)
+         set
+         (set/difference fabric-ids)
+         first)))
